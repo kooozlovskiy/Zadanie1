73c67aaedfe255629d95e4a1c11085d3b90e4245
Тимофей Козлов <gr426_kotig@lab-308-9.tomtit-tomsk.ru>
2025-02-04 11:31:33 +0700

    qaz

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks."
+
     }
 })
+
